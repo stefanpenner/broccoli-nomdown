c1ad88a7c0b92164f84fbc967228ef022a90b5f6
Stefan Penner <stefan.penner@gmail.com>
2017-04-03 20:58:23 -0700

    md | markdown -> html

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const Plugin = require('broccoli-persistent-filter');
 const nomdown = require('nomdown');
 
 module.exports  = class extends Plugin {
+  constructor(node, config) {
+    super(node, config);
+    this.extensions = ['md', 'markdown']
+    this.targetExtension = 'html';
+  }
+
   processString(string, releativeFile) {
     return nomdown(string);
   }
